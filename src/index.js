d11ae5ea2af92d4c6f93eb7309b8868439823f9d
mtaylorpdx <taylor.matt@protonmail.com>
2020-05-11 11:57:52 -0700

    add middleware
    Co-authored-by: Zakkrey Short <zakkreyjshort@gmail.com>
    Co-authored-by: Benjamin Thom <jaminthom@gmail.com>
    Co-authored-by:  K. Wicz <k@solovewi.cz>

--- src/index.js
+++ src/index.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
-// import { createStore, applyMiddleware } from 'redux';
-// import reducer from './reducers';
-// import middlewareExample from './middleware/middleware-example';
+import App from './App';
+import { createStore } from 'redux';
+import reducer from './reducers/cities-reducer';
+import { Provider } from 'react-redux';
 
-// const store = createStore(reducer, applyMiddleware(middlewareExample));
+const store = createStore(reducer);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
