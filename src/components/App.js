909c2e57475abe0398e01eb823a0bfab920c0bd8
mtaylorpdx <taylor.matt@protonmail.com>
2020-05-11 10:30:32 -0700

    arrange file structure
    Co-authored-by: Zakkrey Short <zakkreyjshort@gmail.com>
    Co-authored-by: Benjamin Thom <jaminthom@gmail.com>
    Co-authored-by:  K. Wicz <k@solovewi.cz>

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+import './Cities';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Cities />
+    </React.Fragment>
+  );
+}
+
+export default App;
