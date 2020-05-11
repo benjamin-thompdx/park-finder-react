2b145368628f779984b489f4b43fcbd5f64c7bba
mtaylorpdx <taylor.matt@protonmail.com>
2020-05-11 14:09:00 -0700

    refactor to use thunk middleware
    Co-authored-by: Zakkrey Short <zakkreyjshort@gmail.com>
    Co-authored-by: Benjamin Thom <jaminthom@gmail.com>
    Co-authored-by:  K. Wicz <k@solovewi.cz>

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,3 +13,19 @@ export const getCitiesFailure = (error) => ({
   type: c.GET_CITIES_FAILURE,
   error
 });
+
+export const makeApiCall = () => {
+  const searchKey = "dallas";
+  return dispatch => {
+    dispatch(requestCities);
+    return fetch(`https://api.teleport.org/api/cities/?search=${searchKey}`)
+    .then(response => response.json())
+    .then(
+      (jsonifiedResponse) => {
+        dispatch(getCitiesSuccess(jsonifiedResponse.results));
+      })
+    .catch((error) => {
+      dispatch(getCitiesFailure(error));
+    });
+  }
+}
--- src/components/Cities.js
+++ src/components/Cities.js
@@ -1,65 +1,74 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { makeApiCall } from './../actions';
 
 class Cities extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {
-          error: null,
-          isLoaded: false,
-          cities: []
-      };
+      // this.state = {
+      //     error: null,
+      //     isLoaded: false,
+      //     cities: []
+      // };
     }
 
-    makeApiCall = () => {
-      const searchKey = "dallas";
-      fetch(`https://api.teleport.org/api/cities/?search=${searchKey}`)
-        .then(response => response.json())
-        .then(
-          (jsonifiedResponse) => {
-            this.setState({
-              isLoaded: true,
-              cities: jsonifiedResponse._embedded["city:search-results"]
-            });
-          })
-          .catch((error) => {
-            this.setState({
-              isLoaded: true,
-              error
-            });
-          });
-    }
+    // makeApiCall = () => {
+    //   const searchKey = "dallas";
+    //   fetch(`https://api.teleport.org/api/cities/?search=${searchKey}`)
+    //     .then(response => response.json())
+    //     .then(
+    //       (jsonifiedResponse) => {
+    //         this.setState({
+    //           isLoaded: true,
+    //           cities: jsonifiedResponse._em  bedded["city:search-results"]
+    //         });
+    //       })
+    //       .catch((error) => {
+    //         this.setState({
+    //           isLoaded: true,
+    //           error
+    //         });
+    //       });
+    // }
     
     componentDidMount() {
-      this.makeApiCall()
+      const { dispatch } = this.props;
+      dispatch(makeApiCall());
     }
     
     render(){
-      const { error, isLoaded, cities } = this.state;
+      const { error, isLoading, cities } = this.props;
       console.log("CITIES: ", cities);
       if (error) {
         return <React.Fragment>Error: {error.message}</React.Fragment>;
-      } else if (!isLoaded) {
+      } else if (isLoading) {
         return <React.Fragment>Loading...</React.Fragment>;
       } else {
-        const citiesArray = cities[0];
-        console.log("CITIES ARRAY", citiesArray);
-        console.log(Object.values(cities))
-        const testArray = Object.values(cities);
+        // console.log(Object.values(cities))
+        // const testArray = Object.values(cities);
         return (
           <React.Fragment>
             <h1>City results</h1>
-            <ul>
+            {/* <ul>
               {testArray.map((city, index) =>
               <li key={index}>
                 <h4>{city.matching_full_name}</h4>
               </li>
               )}
-            </ul>
+            </ul> */}
           </React.Fragment>
         );
       }
     }
   }
+
+  const mapStateToProps = state => {
+    return {
+      cities: state.cities,
+      isLoading: state.isLoading,
+      error: state.error
+    }
+  }
     
     
-export default Cities;
+  export default connect(mapStateToProps)(Cities);
